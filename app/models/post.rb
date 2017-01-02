b4db1a8e22d3deec2b7b068f526e6f54aa51a6b8
Rob Hitt <robhitt@gmail.com>
2017-01-01 22:11:02 -0500

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"], message: "Must choose Fiction or Non-Fiction, %{value} is not a valid category" }
+  validates :content, length: { minimum: 100 }
 end
